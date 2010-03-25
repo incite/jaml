a21f50b3588b6f6759a84c50fe7a1397f113003c
Aaron Peckham <apeckham@gmail.com>
2010-03-25 12:20:44 -0700

    added hello world jaml test

---
+++ specs/jaml_specs.js
@@ -0,0 +1,9 @@
+describe("Jaml", function () {
+  it("should render hello world", function() {
+    Jaml.register("my_template", function() {
+      p("hello world");
+    });
+    
+    expect(Jaml.render("my_template")).toEqual("<p>hello world</p>\n");
+  });
+});
