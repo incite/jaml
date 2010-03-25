32a436dc635bda7dac10b13e8f550f2f7c5b9534
Aaron Peckham <apeckham@gmail.com>
2010-03-25 12:26:11 -0700

    add spec from README

--- specs/jaml_specs.js
+++ specs/jaml_specs.js
@@ -6,4 +6,37 @@ describe("Jaml", function () {
     
     expect(Jaml.render("my_template")).toEqual("<p>hello world</p>\n");
   });
+  
+  it("should render a simple template", function() {
+    Jaml.register('simple', function() {
+      div(
+        h1("Some title"),
+        p("Some exciting paragraph text"),
+        br(),
+
+        ul(
+          li("First item"),
+          li("Second item"),
+          li("Third item")
+        )
+      );
+    });
+    
+    expect(clean(Jaml.render('simple'))).toEqual(clean("\
+      <div>\
+        <h1>Some title</h1>\
+        <p>Some exciting paragraph text</p>\
+        <br />\
+        <ul>\
+          <li>First item</li>\
+          <li>Second item</li>\
+          <li>Third item</li>\
+        </ul>\
+      </div>\
+    "));
+  })
 });
+
+function clean(string) {
+  string.replace(/>\s+</, "><");
+}
